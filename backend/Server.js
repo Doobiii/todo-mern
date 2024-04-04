6dc4299cda26291cf8c673c78dcdc81a0f5260ef
Doobiii <Doobiii>
2024-04-04 13:30:53 +0530

    server

--- backend/Server.js
+++ backend/Server.js
@@ -8,16 +8,16 @@ const list = require("./routes/list");
 require("./connection/conn");
 
 app.use(express.json());
-app.use(cors(
-  {
-    origin:["https://todo-mern-front.vercel.app"],
-    methods:["POST","GET"],
-    credentials:true
-  }
-));
-// app.get("/", (req, res) => {
-//   res.send("hi");
-// });
+app.use(
+  cors({
+    origin: ["https://todo-mern-front.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+app.get("/", (req, res) => {
+  res.send("hi");
+});
 
 app.use("/api/v1", auth);
 app.use("/api/v2", list);
